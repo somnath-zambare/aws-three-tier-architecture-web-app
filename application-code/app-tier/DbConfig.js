4e50b123d50dfd1ff0a14df372cc3c55bbccf936
Somnath Zambare <somnathgzambare11@gmail.com>
2024-09-16 17:14:08 +0530

    first commit

---
+++ application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.cluster-cvsscqk80oqr.us-east-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'database1234',
+    DB_DATABASE : 'webappdb'
+});
